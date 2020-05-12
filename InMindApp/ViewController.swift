3e067ecec50dfd9c81aa6ba2a5474831594b0575
Solant Dorian <dorian_du_83@hotmail.fr>
2020-05-12 20:35:47 +0200

    Initial Commit

---
+++ InMindApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InMindApp
+//
+//  Created by Dorian Solant on 12/05/2020.
+//  Copyright © 2020 Dorian Solant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
