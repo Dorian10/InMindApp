9c7237097b9a4035ca150c692e5ee3bdc903e80f
Solant Dorian <dorian_du_83@hotmail.fr>
2020-05-13 04:54:36 +0200

    Ajout de la Tab Bar et du Navigation Controller.

---
+++ InMindApp/Controller/ChapterController.swift
@@ -0,0 +1,30 @@
+//
+//  ChapterController.swift
+//  InMindApp
+//
+//  Created by Dorian Solant on 13/05/2020.
+//  Copyright © 2020 Dorian Solant. All rights reserved.
+//
+
+import UIKit
+
+class ChapterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
